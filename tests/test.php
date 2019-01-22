ed32bf6df51a7e0b8834a44f964c405d6df436cf
Renan Medina <renan@silvamedina.com.br>
2019-01-22 20:13:34 -0200

    Removed enums, not actually related to the provider itself

--- tests/test.php
+++ tests/test.php
@@ -1,2 +1,8 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
+use Bitcastle\OneSignal\OneSignalService;
+use Bitcastle\OneSignal\OneSignalNotification;
+
+$notification = new OneSignalNotification("Testing Message", "Testing Title");
+$client = new OneSignalService();
+echo "Notification and Client instantiated, namespace works";
